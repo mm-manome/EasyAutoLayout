59e97194fa44841cdc35f3c7b10bc35c2e91ee86
Masataka Miyoshi <masataka.ehime.univ@gmail.com>
2018-02-06 23:40:45 +0900

    Initial Commit

---
+++ EasyAutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyAutoLayout
+//
+//  Created by Miyoshi Masataka on 2018/02/06.
+//  Copyright © 2018年 Masataka Miyoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
